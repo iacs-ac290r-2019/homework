0d2bc919d83f788dc3b6a1eb67d91105b19299f5
yue-stephanie <30400317+yue-stephanie@users.noreply.github.com>
2019-03-12 13:40:33 -0400

    changed plot linestyle, linewidth, and padded names

--- project1/viz/frame.py
+++ project1/viz/frame.py
@@ -12,11 +12,15 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 import netCDF4
 
-ny = 65 # subject to change 1025
-nx = 65 # subject to change 2049
+# set all contour lines to be solid
+# u_x u_y fields some contour lines are dottted to indicate negativity
+plt.rcParams['contour.negative_linestyle'] = 'solid'
+
+ny = 1025 # subject to change 1025
+nx = 2049 # subject to change 2049
 
 # open the Exodus file
-path = 'test.exo' # subject to change on Odyssey
+path = 'case1.exo' # subject to change on Odyssey
 ray = netCDF4.Dataset(path)
 
 # mapping between NetCDF variable names and actual meaning
@@ -36,16 +40,17 @@ y_2d = np.reshape(y[:], [ny, nx])
 t_1d = np.reshape(t[:], [1, niters])
 
 # function that save the temperature profile at a frame
-def save_temp_frame(i):
+def save_temp_frame(frame_number):
     fig = plt.figure(figsize=[15,15])
     ax = fig.add_subplot(111)
-    cs = ax.contourf(x_2d,y_2d,temp_i,100,cmap=plt.cm.get_cmap('RdBu'))
-    cs2 = ax.contour(cs, levels=cs.levels[::10], colors='k',linewidths=1.0, alpha=0.6)
+    ax.pcolormesh(x_2d,y_2d,temp_i,cmap=plt.cm.get_cmap('jet'))
+    # cs = ax.contourf(x_2d,y_2d,temp_i,100,cmap=plt.cm.get_cmap('jet'))
+    # cs2 = ax.contour(cs, levels=cs.levels[::10], colors='k',linewidths=2.0)
     ax.set_xlabel(r'$x$', fontsize=16)
     ax.set_ylabel(r'$y$', fontsize=16)
     ax.set_title('Temperature at Frame %i' % i, fontsize=20)
     ax.set_aspect(1.0)
-    plt.savefig('temp_frames/frame' + str(frame_number) + '.png')
+    plt.savefig('temp_frames/frame%04d.png' % frame_number)
     plt.close('all')
     # plt.show()
 
@@ -53,13 +58,13 @@ def save_temp_frame(i):
 def save_ux_frame(frame_number):
     fig = plt.figure(figsize=[15,15])
     ax = fig.add_subplot(111)
-    cs = ax.contourf(x_2d,y_2d,ux_i,100,cmap=plt.cm.get_cmap('RdBu'))
-    cs2 = ax.contour(cs, levels=cs.levels[::10], colors='k',linewidths=1.0, alpha=0.6)
+    cs = ax.contourf(x_2d,y_2d,ux_i,100,cmap=plt.cm.get_cmap('jet'))
+    cs2 = ax.contour(cs, levels=cs.levels[::10], colors='k',linewidths=2.0)
     ax.set_xlabel(r'$x$', fontsize=16)
     ax.set_ylabel(r'$y$', fontsize=16)
     ax.set_title('U_x at Frame %i' % i, fontsize=20)
     ax.set_aspect(1.0)
-    plt.savefig('temp_frames/frame' + str(frame_number) + '.png')
+    plt.savefig('ux_frames/frame%04d.png' % frame_number)
     plt.close('all')
     # plt.show()
 
@@ -67,38 +72,40 @@ def save_ux_frame(frame_number):
 def save_uy_frame(frame_number):
     fig = plt.figure(figsize=[15,15])
     ax = fig.add_subplot(111)
-    cs = ax.contourf(x_2d,y_2d,uy_i,100,cmap=plt.cm.get_cmap('RdBu'))
-    cs2 = ax.contour(cs, levels=cs.levels[::10], colors='k',linewidths=1.0, alpha=0.6)
+    cs = ax.contourf(x_2d,y_2d,uy_i,100,cmap=plt.cm.get_cmap('jet'))
+    cs2 = ax.contour(cs, levels=cs.levels[::10], colors='k',linewidths=2.0)
     ax.set_xlabel(r'$x$', fontsize=16)
     ax.set_ylabel(r'$y$', fontsize=16)
     ax.set_title('U_y at Frame %i' % i, fontsize=20)
     ax.set_aspect(1.0)
-    plt.savefig('temp_frames/frame' + str(frame_number) + '.png')
+    plt.savefig('uy_frames/frame%04d.png' % frame_number)
     plt.close('all')
     # plt.show()
 
 def save_p_frame(frame_number):
     fig = plt.figure(figsize=[15,15])
     ax = fig.add_subplot(111)
-    cs = ax.contourf(x_2d,y_2d,p_i,100,cmap=plt.cm.get_cmap('RdBu'))
-    cs2 = ax.contour(cs, levels=cs.levels[::10], colors='k',linewidths=1.0, alpha=0.6)
+    cs = ax.contourf(x_2d,y_2d,p_i,100,cmap=plt.cm.get_cmap('jet'))
+    cs2 = ax.contour(cs, levels=cs.levels[::10], colors='k',linewidths=2.0)
     ax.set_xlabel(r'$x$', fontsize=16)
     ax.set_ylabel(r'$y$', fontsize=16)
     ax.set_title('Pressure at Frame %i' % i, fontsize=20)
     ax.set_aspect(1.0)
-    plt.savefig('temp_frames/frame' + str(frame_number) + '.png')
+    plt.savefig('p_frames/frame%04d.png' % frame_number)
     plt.close('all')
     # plt.show()
 
 # save all temperature fields
-for i in range(niters):
+for i in range(0,50):
     # load variables step by step
-    p_i = p[i].reshape(ny,nx)
-    temp_i = temp[i].reshape(ny,nx)
+    # p_i = p[i].reshape(ny,nx)
+    # temp_i = temp[i].reshape(ny,nx)
     ux_i = ux[i].reshape(ny,nx)
-    uy_i = uy[i].reshape(ny,nx)
-#     save_temp_frame(i)
-#     save_ux_frame(i)
+    # uy_i = uy[i].reshape(ny,nx)
+    # save_temp_frame(i)
+    save_ux_frame(i)
 #     save_uy_frame(i)
 #     save_p_frame(i)
-    print("Figures saved at Frame " + str(i))
+# color the temp by velocity or 
+# color the streamline by velocity_magnitude/temp
+    print("Figures saved at Frame %04d" % i)
