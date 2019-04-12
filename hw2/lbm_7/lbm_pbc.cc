b979283dee61a07e6ca2d31df566504c989ea801
yue-stephanie <30400317+yue-stephanie@users.noreply.github.com>
2019-04-12 18:51:17 -0400

    added main script for part 1 periodic bc

---
+++ hw2/lbm_7/lbm_pbc.cc
@@ -0,0 +1,37 @@
+#include <cstdio>
+
+#include "lbm.hh"
+
+int main() {
+    char odir[]="lbm_pbc_Re001.out"; // output directory
+
+    /** This is a simple Poiseuille flow.
+     *  Periodic boundary condition on west and east.
+     *  No-slip on north and south.
+     *  initial condition: rho_0 = 1.0, ux_0 = 0.0, uy_0 = 0.0
+     *  Expected outcome: uy stays at 0.0, density stays at 1.0,
+     *                    ux has parabolic behavior at vertical cut.
+     *  forcing gives the velocity. 
+     *  Run 15000 time (relative) and output every 150 frame. */
+
+    // Set up simulation parameters.
+    double Re=0.01;      // Reynolds number
+    double tau=1;    // Relaxation constant
+    double D=60;	   // Reference length
+    // Set up grid properties.
+    int nx=200;		   // Channel length
+    int ny=60;		   // Channel width	
+    int w=30;          // Narrowing width
+    int l=50;          // Narrowing length
+    int flowtype=0;    // 0: Static flow, 1: Poiseuille flow
+    int bctype=0;      // 0: Periodic, 1: Inlet-Outlet
+    double forcetype=0.0001; // Poiseuille flow, NOT USED THOUGH
+    
+    // Create the simulation domain with the specified parameters and dimensions
+    lbm fl(Re,tau,D,nx,ny,odir);    
+
+    // Call initialization functions to create simulation region and set up the initial condition
+    fl.initialize(1.,0.,0.,flowtype);
+    fl.solve(15000,100,bctype,forcetype,w,l);
+    printf("Ran for 15000 iterations. Saved output every 150 frames. Finished!\n");
+}
