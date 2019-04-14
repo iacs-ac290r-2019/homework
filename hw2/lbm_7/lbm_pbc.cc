a3d0282a092ab8423178a338068d57dfe095d8e3
yue-stephanie <30400317+yue-stephanie@users.noreply.github.com>
2019-04-14 01:04:58 -0400

    changed inputs to match problem statement, fixed force equation

--- hw2/lbm_7/lbm_pbc.cc
+++ hw2/lbm_7/lbm_pbc.cc
@@ -6,34 +6,41 @@ int main() {
     char odir[]="lbm_pbc_Re001.out"; // output directory
 
     /** This is a simple Poiseuille flow.
+
+     *  Re = 0.01
+     *  nu = 0.1 -> tau = 0.8
+     *  umax = Re*nu/D = 0.01*0.1/60 = 0.001/60
+     *  mu = rho*nu
+     *  f = 8*mu*umax/H/H = 8*0.1*0.001/60/60/60
+
      *  Periodic boundary condition on west and east.
      *  No-slip on north and south.
      *  initial condition: rho_0 = 1.0, ux_0 = 0.0, uy_0 = 0.0
      *  Expected outcome: uy stays at 0.0, density stays at 1.0,
      *                    ux has parabolic behavior at vertical cut.
      *  forcing gives the velocity. 
-     *  Run 15000 time (relative) and output every 150 frame. */
+     *  Run 10000 time (relative) and output every 100 frame. */
 
     // Set up simulation parameters.
     double Re=0.01;      // Reynolds number
-    double tau=1;    // Relaxation constant
+    double tau=0.8;    // Relaxation constant
     double D=60;	   // Reference length
     // Set up grid properties.
     int nx=200;		   // Channel length
     int ny=60;		   // Channel width	
     int w=30;          // Narrowing width
     int l=50;          // Narrowing length
-    int flowtype=0;    // 0: Static flow, 1: Poiseuille flow
-    int bctype=0;      // 0: Periodic, 1: Inlet-Outlet
-    double forcetype=0.0001; // Poiseuille flow, NOT USED THOUGH
-    
+    int flowtype=0;    // 0: Standard, 1: Inlet-Outlet
+    int bctype=0;      // 0: Periodic, 1: Open
     // Create the simulation domain with the specified parameters and dimensions
-    lbm fl(Re,tau,D,nx,ny,odir);    
+    lbm fl(Re,tau,D,nx,ny,odir);
+    double umax=Re*fl.nu/D;
+    double forcetype=8*fl.nu*umax/D/D;
 
     // Call initialization functions to create simulation region and set up the initial condition
     fl.initialize(1.,0.,0.,flowtype);
-    fl.solve(15000,100,bctype,forcetype,w,l);
-    printf("Ran for 15000 iterations. Saved output every 150 frames. Finished!\n");
+    fl.solve(10000,100,bctype,forcetype,w,l);
+    printf("Ran for 10000 iterations. Saved output every 100 frames. Finished!\n");
 
     // Run multiple passes for different widths w
     for (int w=10; w<=50; w += 10)
@@ -47,8 +54,8 @@ int main() {
 
         // Call initialization functions to create simulation region and set up the initial condition
         fl.initialize(1.,0.,0.,flowtype);
-        fl.solve(15000,100,bctype,forcetype,w,l);
-        printf("Ran for 15000 iterations with w=%d. Saved output every 150 frames. Finished!\n", w);
+        fl.solve(10000,100,bctype,forcetype,w,l);
+        printf("Ran for 10000 iterations with w=%d. Saved output every 100 frames. Finished!\n", w);
     }
 
     // Run multiple passes for different kinematic viscosities nu
@@ -65,10 +72,12 @@ int main() {
 
         // Create the simulation domain with the specified parameters and dimensions
         lbm fl(Re,tau,D,nx,ny,odir);
+        double umax=Re*fl.nu/D;
+        double forcetype=8*fl.nu*umax/D/D;
 
         // Call initialization functions to create simulation region and set up the initial condition
         fl.initialize(1.,0.,0.,flowtype);
-        fl.solve(15000,100,bctype,forcetype,w,l);
-        printf("Ran for 15000 iterations with tau=%0.2f. Saved output every 150 frames. Finished!\n", tau);
+        fl.solve(10000,100,bctype,forcetype,w,l);
+        printf("Ran for 10000 iterations with tau=%gf. Saved output every 100 frames. Finished!\n", tau);
     }
 }
