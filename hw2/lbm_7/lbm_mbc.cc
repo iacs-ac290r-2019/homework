f2d628884f0fe6649ea6b882f7baca1a1c413bdf
yue-stephanie <30400317+yue-stephanie@users.noreply.github.com>
2019-04-14 02:44:46 -0400

    updated inputs in obc driver script

--- hw2/lbm_7/lbm_mbc.cc
+++ hw2/lbm_7/lbm_mbc.cc
@@ -6,37 +6,45 @@ int main() {
     char odir[]="lbm_mbc_Re001.out"; // output directory
 
     /** This is a simple Poiseuille flow.
-     *  With pressure gradient on west and east.
+
+     *  Re = 0.01
+     *  nu = 0.1 -> tau = 0.8
+     *  umax = Re*nu/D = 0.01*0.1/60 = 0.001/60
+     *  mu = rho*nu
+     *  f = 8*mu*umax/H/H = 8*0.1*0.001/60/60/60
+
+     *  Periodic boundary condition on west and east.
      *  No-slip on north and south.
-     *  initial condition: rho_0 = 1.0, ux_0 = Re*nu/D, uy_0 = 0.0
+     *  initial condition: rho_0 = 1.0, ux_0 = 0.0, uy_0 = 0.0
      *  Expected outcome: uy stays at 0.0, density stays at 1.0,
      *                    ux has parabolic behavior at vertical cut.
-     *  No forcing. 
-     *  Run 2000 time (relative) and output every 20 frame. */
+     *  forcing gives the velocity. 
+     *  Run 10000 time (relative) and output every 100 frame. */
 
     // Set up simulation parameters.
     double Re=0.01;      // Reynolds number
-    double tau=1;    // Relaxation constant
+    double tau=0.8;    // Relaxation constant
     double D=60;       // Reference length    
     // Set up grid properties.
     int nx=200;        // Channel length
     int ny=60;         // Channel width 
     int w=30;          // Narrowing width
     int l=50;          // Narrowing length
-    int flowtype=1;    // 0: No density input-output, 1: Inlet-Outlet
-    int bctype=1;      // 0: Periodic, 1: Mixed
+    int flowtype=1;    // 0: Standard, 1: Inlet-Outlet
+    int bctype=1;      // 0: Periodic, 1: Open
     double forcetype=0.;
     
     // Create the simulation domain with the specified parameters and dimensions
     lbm fl(Re,tau,D,nx,ny,odir);
 
     // Calculate the input velocity
-    double u0=Re*fl.nu/D;
+    // double u0=Re*fl.nu/D;
+    // printf("Sanity check u0: %g\n",u0);
 
     // Call initialization functions to create simulation region and set up the initial condition
-    fl.initialize(1.,u0,0.,flowtype);
+    fl.initialize(1.,0.,0.,flowtype);
     // double forcetype=0.0001;
-    fl.solve(2000,100,bctype,forcetype,w,l);
+    fl.solve(1000,100,bctype,forcetype,w,l);
     printf("Ran for 2000 iterations. Saved output every 20 frames. Finished!\n");
 
     // Run multiple passes for different widths w
